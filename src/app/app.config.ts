a66c35c2db0c50f8eb377b2d3b354c746f455cd9
Bruno Barross <altamiro_bruno@yahoo.com.br>
2025-02-06 15:41:06 -0300

    feat: :sparkles: define lime como cor primaria no primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,25 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/lara';
-import Material from '@primeng/themes/material';
+import { definePreset } from '@primeng/themes';
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{lime.50}',
+      100: '{lime.100}',
+      200: '{lime.200}',
+      300: '{lime.300}',
+      400: '{lime.400}',
+      500: '{lime.500}',
+      600: '{lime.600}',
+      700: '{lime.700}',
+      800: '{lime.800}',
+      900: '{lime.900}',
+      950: '{lime.950}',
+    },
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,12 +33,12 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Material,
+        preset: Aura,
         options: {
           prefix: 'p',
           darkModeSelector: 'p-dark',
-          cssLayer: false
-      }
+          cssLayer: false,
+        },
       },
     }),
   ],
