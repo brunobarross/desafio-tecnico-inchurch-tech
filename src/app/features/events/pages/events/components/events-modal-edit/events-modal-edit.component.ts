64dbf23f65dc5b393057c8ab93c66fa30144e265
Bruno Barross <altamiro_bruno@yahoo.com.br>
2025-02-07 10:00:34 -0300

    feat: aprimora modal de edição de eventos com novos campos e validação

--- src/app/features/events/pages/events/components/events-modal-edit/events-modal-edit.component.ts
+++ src/app/features/events/pages/events/components/events-modal-edit/events-modal-edit.component.ts
@@ -2,15 +2,39 @@ import { Component, signal } from '@angular/core';
 import { Dialog } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { RadioButton } from 'primeng/radiobutton';
 
 @Component({
   selector: 'app-events-modal-edit',
-  imports: [Dialog, ButtonModule, InputTextModule],
+  imports: [
+    Dialog,
+    ButtonModule,
+    InputTextModule,
+    TextareaModule,
+    ReactiveFormsModule,
+    RadioButton,
+  ],
   templateUrl: './events-modal-edit.component.html',
-  styleUrl: './events-modal-edit.component.scss'
+  styleUrl: './events-modal-edit.component.scss',
 })
 export class EventsModalEditComponent {
+  constructor(private formBuilder: FormBuilder) {}
   visible = signal<boolean>(true);
-
-
+  formEditEvent!: FormGroup;
+  ngOnInit() {
+    this.formEditEvent = this.formBuilder.group({
+      title: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      date: new FormControl('', Validators.required),
+      status: new FormControl('', Validators.required),
+    });
+  }
 }
